b71fb365400b6d207dd84067cb87a30c889cc922
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2024-07-24 19:24:51 -0600

    Update securityFilterChain

--- src/main/java/dev/angelcruzl/users/app/auth/SpringSecurityConfig.java
+++ src/main/java/dev/angelcruzl/users/app/auth/SpringSecurityConfig.java
@@ -14,9 +14,13 @@ public class SpringSecurityConfig {
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
-            .authorizeHttpRequests(auth ->
-                auth.requestMatchers(HttpMethod.GET, "/api/users/**").permitAll()
-                    .anyRequest().authenticated()
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers(HttpMethod.GET, "/api/users*").permitAll()
+                .requestMatchers(HttpMethod.POST, "/api/users").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.GET, "/api/users/{id}").hasAnyRole("ADMIN", "USER")
+                .requestMatchers(HttpMethod.PUT, "/api/users/{id}").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.DELETE, "/api/users/{id}").hasRole("ADMIN")
+                .anyRequest().authenticated()
             ).csrf(AbstractHttpConfigurer::disable)
             .sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .build();
